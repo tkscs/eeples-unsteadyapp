034e0237243cfe03af0ea183f27ee2b872738cd5
unsteadyapp <akanodia2029@kehillahstudent.org>
2026-01-27 10:25:54 -0800

    diffrent methods

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 song = "I like to eat, eat, eat, apples and bananas"
-
+typeOf = 3
 def sing(vowel):
-    print(song.replace("a",vowel))
-
+    if(typeOf == 1):
+        print(song.replace("a",vowel))
+    elif(typeOf == 2):
+        newString = ""
+        for i in song:
+            if(i == "a"):
+                newString += vowel
+            else:
+                newString += i
+        print(newString)
+    elif typeOf == 3:
+        for i in song:
+            print((i * (i!="a")) + ((vowel)* (i=="a")),end="")
+        print("")
 sing("ay")
 sing("ee")
 sing("i")
