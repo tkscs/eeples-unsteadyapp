3ba28b609245b17975200940d2cbc204bd39c391
unsteadyapp <akanodia2029@kehillahstudent.org>
2026-01-29 12:00:46 -0800

    changes with a repeat

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 song = "I like to eat, eat, eat, apples and bananas"
+print(len(song))
 typeOf = 3
 def sing(vowel):
     if(typeOf == 1):
@@ -15,6 +16,12 @@ def sing(vowel):
         for i in song:
             print((i * (i!="a")) + ((vowel)* (i=="a")),end="")
         print("")
+def repeat(string):
+    newstring = ""
+    for i in string:
+        newstring += i * 3
+    return newstring
+print(repeat(song))
 sing("ay")
 sing("ee")
 sing("i")
